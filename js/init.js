c58c01b84f85d1ba6f3056f86c6ccc6cbc3719ad
Logan Kojiro <logankojiro@Logans-MacBook-Pro.local>
2018-05-22 13:10:21 -0700

    new frontpage,fixed a bunch of JS

--- js/init.js
+++ js/init.js
@@ -6,14 +6,9 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $('.collapsible').collapsible();
-    $('.chart').easyPieChart({
-      animate: 5000,
-      scaleColor: false,
-      size: 160,
-      lineWidth: 25,
-      lineCap: 'butt',
-      barColor: "#00838f"
-  });
+    $('.scrollspy').scrollSpy();
+    $('.fixed-action-btn').floatingActionButton();
+    $('.tooltipped').tooltip();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
